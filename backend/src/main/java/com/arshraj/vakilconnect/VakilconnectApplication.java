d33bbf8053d11fa78b88fa3995ddf7bc746b43ce
Arsh Raj <YOUR_GITHUB_EMAIL>
2026-06-22 20:24:33 +0530

    feat: initialize Spring Boot backend structure

---
+++ backend/src/main/java/com/arshraj/vakilconnect/VakilconnectApplication.java
@@ -0,0 +1,13 @@
+package com.arshraj.vakilconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VakilconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VakilconnectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/arshraj/vakilconnect/VakilconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arshraj.vakilconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VakilconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
