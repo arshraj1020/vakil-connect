81715f96837ca1ad8002704b4c88cebab8d6d1e1
Arsh Raj <YOUR_GITHUB_EMAIL>
2026-06-30 15:24:41 +0530

    feat: implement user registration service

--- backend/src/main/java/com/arshraj/vakilconnect/user/service/UserService.java
+++ backend/src/main/java/com/arshraj/vakilconnect/user/service/UserService.java
@@ -1,5 +1,10 @@
 package com.arshraj.vakilconnect.user.service;
 
+import com.arshraj.vakilconnect.user.dto.RegisterUserRequest;
+import com.arshraj.vakilconnect.user.entity.User;
+
 public interface UserService {
+
+    User registerUser(RegisterUserRequest request);
 
 }
--- backend/src/main/java/com/arshraj/vakilconnect/user/service/UserServiceImpl.java
+++ backend/src/main/java/com/arshraj/vakilconnect/user/service/UserServiceImpl.java
@@ -1,8 +1,29 @@
 package com.arshraj.vakilconnect.user.service;
 
+import com.arshraj.vakilconnect.user.dto.RegisterUserRequest;
+import com.arshraj.vakilconnect.user.entity.User;
+import com.arshraj.vakilconnect.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User registerUser(RegisterUserRequest request) {
+
+        User user = new User();
+
+        user.setFullName(request.getFullName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setPhoneNumber(request.getPhoneNumber());
+
+        return userRepository.save(user);
+    }
 }
